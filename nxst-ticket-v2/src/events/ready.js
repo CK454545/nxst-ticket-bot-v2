6cad5d5704d899086600dfb2f9a92f46879299ca
CKxAI <gaetanck.pro@gmail.com>
2025-07-19 23:01:01 +0200

    Delete nxst-ticket-v2 directory

--- nxst-ticket-v2/src/events/ready.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  name: 'ready',
-  once: true,
-  async execute(client) {
-    console.log(`🤖 Bot prêt et connecté en tant que ${client.user.tag}`);
-  }
-};
--- nxst-ticket-v2/src/utils/permissions.js
+++
@@ -1,5 +0,0 @@
-function isTicketOwnerOrStaff(member, ticketOwnerId, rolesStaff) {
-  return member.id === ticketOwnerId || member.roles.cache.some(r => rolesStaff.includes(r.id));
-}
-
-module.exports = { isTicketOwnerOrStaff };
